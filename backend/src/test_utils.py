ea9da523740df00aaf868abc61f4996d814665c9
samuellipovetsky <samuellpvt@gmail.com>
2024-05-24 15:24:16 -0300

    yaml

--- backend/src/test_utils.py
+++ backend/src/test_utils.py
@@ -3,7 +3,7 @@ import pytest
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-from utils import *
+from utils import Transcriber
 from collections import namedtuple
 
 
